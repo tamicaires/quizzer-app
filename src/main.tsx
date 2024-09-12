39bce3e6b4ed7a167973611c7d7ecd153fc77d8d
tamicaires <tamiresdec@gmail.com>
2024-09-12 14:28:29 -0300

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import "./globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>
 );
